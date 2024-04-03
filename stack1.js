e94458283fc917c28d71ef6a10a6b9ab92b73e9e
rsKaurav98 <kauravrohit98@gmail.com>
2024-04-03 15:36:31 +0530

    first qestion of stack

---
+++ stack1.js
@@ -0,0 +1,22 @@
+function NGR(arr) {
+    const result = [];
+    const stack = [];
+    
+    for (let i = arr.length - 1; i >= 0; i--) {
+        while (stack.length > 0 && stack[stack.length - 1] <= arr[i]) {
+            stack.pop();
+        }
+        if (stack.length === 0) {
+            result.unshift(-1); 
+        } else {
+            result.unshift(stack[stack.length - 1]);
+        }
+        stack.push(arr[i]);
+    }
+    
+    return result;
+}
+
+
+const arr = [4, 5, 2, 10, 8,12,14,2,1];
+console.log(NGR(arr)); 
